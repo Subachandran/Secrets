55a04b5426f0626d1b04a0ec95458815898832a8
Subachandran-jks <122013047@sastra.ac.in>
2022-12-13 12:07:09 +0530

    Confirm password validation

--- public/register-script.js
+++ public/register-script.js
@@ -1,24 +1,10 @@
 $(document).ready(function () {
   $("#confirmPassword").on("keyup", () => {
     if ($("#confirmPassword").val() !== $("#createPassword").val()) {
-      $("#confirmPassword").css(
-        "box-shadow",
-        "0 0 0 0.25rem rgb(255 0 0 / 25%)"
-      );
-      $("label[for='confirmPassword']").html("Confirm Password (Not Matched)");
-      $("label[for='confirmPassword']").css("color", "red");
+      $("#confirmPassword").addClass("is-invalid");
     } else {
-      $("#confirmPassword:focus + label[for='confirmPassword']").html("Confirm Password (Matched)");
-      $("#confirmPassword:focus + label[for='confirmPassword']").css("color", "green");
-      $("#confirmPassword:focus").css(
-        "box-shadow",
-        "0 0 0 0.25rem rgb(0 255 0 / 25%)"
-      );
-      $("#confirmPassword:not(:focus)").css(
-        "box-shadow",
-        "0 0 0 0.25rem rgb(13 110 253 / 25%)"
-      );
+      $(".subBtn").removeClass("disabled");
+      $("#confirmPassword").removeClass("is-invalid").addClass("is-valid");
     }
-    // alert($("#confirmPassword").val())
   });
 });
