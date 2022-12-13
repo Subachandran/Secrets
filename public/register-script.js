8de494d19cf28e21438893c9198f521f913acb87
Subachandran-jks <122013047@sastra.ac.in>
2022-12-13 12:15:45 +0530

    Validation feedback

--- public/register-script.js
+++ public/register-script.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
   $("#confirmPassword").on("keyup", () => {
     if ($("#confirmPassword").val() !== $("#createPassword").val()) {
       $("#confirmPassword").addClass("is-invalid");
+      $(".invalid-feedback").show()
+      $(".valid-feedback").hide()
     } else {
       $(".subBtn").removeClass("disabled");
       $("#confirmPassword").removeClass("is-invalid").addClass("is-valid");
+      $(".invalid-feedback").hide()
+      $(".valid-feedback").show()
     }
   });
 });
